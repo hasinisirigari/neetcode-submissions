a137843cf2ca90bcf1f40e7b36f4665fe3b1905c
hasinisirigari <hasinisirigari93@gmail.com>
2026-09-10 10:06:25 -0400

    Add: string-encode-and-decode - submission-5

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-5.py
@@ -0,0 +1,98 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        encoded=''
+        for s in strs:
+            encoded += str(len(s))+'#'+s
+        return encoded
+    
+    def decode(self, s: str) -> List[str]:
+        decoded,i=[],0
+        while i<len(s):
+            j=i+1
+            while s[j]!='#':
+                j+=1
+            length=int(s[i:j])
+            decoded.append(s[j+1:j+1+length])
+            i=j+1+length
+        return decoded
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    #     enc=''
+    #     for s in strs:
+    #         enc+=str(len(s))+"#"+s
+    #     return enc
+
+    # def decode(self, s: str) -> List[str]:
+    #     res,i=[],0
+    #     while i<len(s):
+    #         j=i+1
+    #         while s[j]!='#':
+    #             j+=1
+    #         length = int(s[i:j])
+    #         res.append(s[j+1:j+1+length])
+    #         i=j+1+length
+
+    #     return res
+            
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+    #     res=''
+    #     for s in strs:
+    #         res+=str(len(s))+'#'+s
+    #     return res
+
+    # def decode(self, s: str) -> List[str]:
+    #     res,i=[],0
+    #     while i<len(s):
+    #         j=i
+    #         while s[j]!='#':
+    #             j+=1
+    #         length=int(s[i:j])
+    #         res.append(s[j+1:j+1+length])
+    #         i=j+1+length
+    #     return res
